32ac2f8efedcb636a0044f96cda3c39983d9694d
Tal144155 <tal.ariel.ziv@gmail.com>
2024-05-22 23:34:34 +0300

    writing the script

---
+++ split_pgn.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+
+# function printing if there are wrong arguments
+wrong_input() {
+    if [ $# -ne 2 ]; then
+        echo "Usage: $0 <source_pgn_file> <destination_directory>"
+        exit 1
+    fi
+}
+
+# function to check if the directory exists
+directory_existing_valid() {
+    if [ ! -d $1 ]; then
+        mkdir -p "$1"
+        echo "Created directory '$1'."
+    fi
+}
+
+# function to check if the file exists
+file_existing_valid() {
+    if [ ! -f $1 ]; then
+        echo "Error: File '$1' does not exist."
+        exit 1
+    fi
+}
+
+#function to split the PGN file
+split_pgn_file() {
+
+    local file=$1
+    local directory=$2
+    local counter=0
+    local new_file=""
+
+    while IFS= read -r line; do
+        # Check if line starts with "[Event"
+        if [[ "$line" =~ ^\[Event\  ]]; then
+            # If yes, create new file
+            ((counter++))
+            new_file="${directory}/$(basename "$file" .pgn)_${counter}.pgn"
+            # Create the new file
+            touch "$new_file"
+            echo "Saved game to $new_file"
+        fi
+        # Append line to current output file if output_file is set
+        if [ -n "$new_file" ]; then
+            echo "$line" >> "$new_file"
+        fi
+    done < "$file"
+    echo "All games have been split and saved to '$directory'."
+}
+
+
+file=$1
+directory=$2
+
+wrong_input "$@"
+
+file_existing_valid "$file"
+
+directory_existing_valid "$directory"
+
+split_pgn_file "$file" "$directory"
+
+
+
