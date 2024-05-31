12601ff00214bff62a122ef972c56da2402f9380
Tal144155 <tal.ariel.ziv@gmail.com>
2024-05-31 12:38:01 +0300

    adding comments

--- split_pgn.sh
+++ split_pgn.sh
@@ -28,23 +28,38 @@ file_existing_valid() {
 #function to split the PGN file
 split_pgn_file() {
     # creating local variables to hold the data needed
-    local file=$1
-    local directory=$2
-    local counter_games=0
-    local new_pgn_file
-    local file_name=$(basename "$file" .pgn)
+    local file=$1                    # store the source PGN file path
+    local directory=$2               # store the destination directory path
+    local counter_games=0            # initialize the game counter
+    local new_pgn_file               # variable to hold the new PGN file path
+    local file_name=$(basename "$file" .pgn)  # extract the file name from the source PGN file path
 
+    # loop through each line in the PGN file
     while IFS= read -r line; do
+        # check if the line starts with '[Event  '
         if [[ "$line" =~ ^\[Event\  ]]; then
+
+            # increment the game counter
             ((counter_games++))
+
+            # create a new PGN file name
             new_pgn_file="${directory}/${file_name}_${counter_games}.pgn"
+
+            # print a message indicating the new file name
             echo "Saved game to $new_pgn_file"
+
+            # create the new PGN file
             touch "$new_pgn_file"
+
+            # write the current line to the new PGN file
             echo "$line" >> "$new_pgn_file"
         else
+            # write the current line to the current PGN file
             echo "$line" >> "$new_pgn_file"
         fi
     done < "$file"
+
+    # print a message indicating that all games have been split and saved
     echo "All games have been split and saved to '$directory'."
 }
 
